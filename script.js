6bb04b013bdc5d67547a2e6c65893d79a99b55a7
Shawn Copeland <shawn.d.copeland@gmail.com>
2018-05-02 20:00:06 -0400

    Sample Data

---
+++ script.js
@@ -0,0 +1,9 @@
+/* #### SVG Shapes for basic manipulation for d3.js charts 
+*/
+
+var sales = [
+    {product: 'Hoodie', count: 30},
+    {product: 'T-shirt', count: 50},
+    {product: 'Pants', count: 43}
+];
+
